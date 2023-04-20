875ad35fcbbafd5f63538d6c2a45a5191f4e6213
Roman Korniienko <roman.korniienko@belkins.io>
2023-04-20 13:22:17 +0300

    chore(lint): update package, ts, eslint and prettier configs; add babel;

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,31 @@
 /* eslint-env node */
-require("@rushstack/eslint-patch/modern-module-resolution");
+require('@rushstack/eslint-patch/modern-module-resolution');
 
 module.exports = {
   root: true,
   env: {
     node: true,
   },
+  rules: {
+    "prettier/prettier": ["error", { "singleQuote": true }]
+  },
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    parser: '@babel/eslint-parser',
+    babelOptions: {
+      parserOpts: {
+        plugins: ['jsx'],
+      },
+    },
+  },
   extends: [
-    "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "@vue/eslint-config-prettier",
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    'prettier',
+    '@vue/eslint-config-prettier',
   ],
 };
+
+// ref: refactor according review;
+//
+// add types for auth api; add new computed; divide function into small parts; fix lint errors;

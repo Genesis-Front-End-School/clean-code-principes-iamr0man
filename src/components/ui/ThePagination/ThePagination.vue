e372518c750d5ac8f139419a2c3c5a605c46fbf1
Roman Korniienko <roman.korniienko@belkins.io>
2023-04-29 12:59:54 +0300

    ref(pagination): rewrite to options api;

--- src/components/ui/ThePagination/ThePagination.vue
+++ src/components/ui/ThePagination/ThePagination.vue
@@ -38,155 +38,156 @@
     </ul>
   </nav>
 </template>
-<script lang="ts" setup>
-import { computed } from 'vue';
+<script lang="ts">
+import { defineComponent } from 'vue';
 import type { PropType } from 'vue';
 import PreviousButton from '@/components/ui/ThePagination/PreviousButton.vue';
 import PageItem from '@/components/ui/ThePagination/PageItem.vue';
 import NextButton from '@/components/ui/ThePagination/NextButton.vue';
 
 export type PaginationLayout = 'navigation' | 'pagination' | 'table';
-
-const emit = defineEmits(['update:modelValue']);
-
-const props = defineProps({
-  modelValue: {
-    type: Number,
-    default: 1,
+export default defineComponent({
+  components: {
+    PreviousButton,
+    PageItem,
+    NextButton,
   },
-  totalPages: {
-    type: Number,
-    default: 1,
+  props: {
+    modelValue: {
+      type: Number,
+      default: 1,
+    },
+    totalPages: {
+      type: Number,
+      default: 1,
+    },
+    perPage: {
+      type: Number,
+      default: 10,
+    },
+    totalItems: {
+      type: Number,
+      required: false,
+    },
+    layout: {
+      type: String as PropType<PaginationLayout>,
+      default: 'pagination',
+    },
+    sliceLength: {
+      type: Number,
+      default: 2,
+    },
+    previousLabel: {
+      type: String,
+      default: 'Previous',
+    },
+    nextLabel: {
+      type: String,
+      default: 'Next',
+    },
   },
-  perPage: {
-    type: Number,
-    default: 10,
+  computed: {
+    computedTotalPages(): number {
+      if (!this.totalItems) return this.totalPages;
+      if (!this.perPage) return this.totalPages;
+
+      return Math.ceil(this.totalItems / this.perPage);
+    },
+    isDecreaseDisabled() {
+      return this.modelValue <= 1;
+    },
+    isIncreaseDisabled() {
+      return this.modelValue >= this.computedTotalPages;
+    },
+    pagesToDisplay(): number[] {
+      if (this.layout === 'navigation') return [];
+      if (this.layout === 'table') return [];
+
+      const sliceLengthCoverAllPages =
+        this.computedTotalPages <= this.sliceLength * 2 + 1;
+      if (sliceLengthCoverAllPages) return this.getTotalPagesArray();
+
+      const withoutSliceLength = this.modelValue <= this.sliceLength;
+      if (withoutSliceLength) return this.getPagesWithoutSlice();
+
+      const endOfPagination =
+        this.modelValue >= this.computedTotalPages - this.sliceLength;
+      if (endOfPagination) return this.getEndOfPagination();
+
+      return this.getPagesWithNewView();
+    },
+    startItemsCount() {
+      return this.modelValue * this.perPage - this.perPage + 1;
+    },
+    endItemsCount() {
+      const count = this.modelValue * this.perPage + 1;
+      if (!this.totalItems) return count;
+      if (count > this.totalItems) return this.totalItems;
+
+      return count;
+    },
+    computedTotalItems() {
+      if (this.totalItems) return this.totalItems;
+
+      return this.computedTotalPages * this.perPage;
+    },
   },
-  totalItems: {
-    type: Number,
-    required: false,
+  methods: {
+    setPage(index: number) {
+      this.$emit('update:modelValue', index);
+    },
+    decreasePage() {
+      this.$emit('update:modelValue', this.modelValue - 1);
+    },
+    increasePage() {
+      this.$emit('update:modelValue', this.modelValue + 1);
+    },
+    getTotalPagesArray() {
+      const pages = [];
+      for (let page = 1; page <= this.computedTotalPages; page++) {
+        pages.push(page);
+      }
+      return pages;
+    },
+    getPagesWithoutSlice() {
+      const pages = [];
+      const slicedLength =
+        Math.abs(this.modelValue - this.sliceLength) +
+        this.modelValue +
+        this.sliceLength +
+        1;
+      for (let page = 1; page <= slicedLength; page++) {
+        pages.push(page);
+      }
+      return pages;
+    },
+    getEndOfPagination() {
+      const pages = [];
+      for (
+        let page = Math.abs(this.computedTotalPages - this.sliceLength * 2);
+        page <= this.computedTotalPages;
+        page++
+      ) {
+        pages.push(page);
+      }
+      return pages;
+    },
+    getPagesWithNewView() {
+      const pages = [];
+      let startedPage =
+        this.modelValue - this.sliceLength > 0
+          ? this.modelValue - this.sliceLength
+          : 1;
+      for (
+        let page = startedPage;
+        page < this.modelValue + this.sliceLength + 1;
+        page++
+      ) {
+        if (page >= this.computedTotalPages) break;
+        pages.push(page);
+      }
+      return pages;
+    },
   },
-  layout: {
-    type: String as PropType<PaginationLayout>,
-    default: 'pagination',
-  },
-  sliceLength: {
-    type: Number,
-    default: 2,
-  },
-  previousLabel: {
-    type: String,
-    default: 'Previous',
-  },
-  nextLabel: {
-    type: String,
-    default: 'Next',
-  },
-});
-
-const setPage = (index: number) => {
-  emit('update:modelValue', index);
-};
-const decreasePage = () => {
-  emit('update:modelValue', props.modelValue - 1);
-};
-const increasePage = () => {
-  emit('update:modelValue', props.modelValue + 1);
-};
-
-const computedTotalPages = computed(() => {
-  if (!props.totalItems) return props.totalPages;
-  if (!props.perPage) return props.totalPages;
-
-  return Math.ceil(props.totalItems / props.perPage);
-});
-
-const isDecreaseDisabled = computed(() => props.modelValue <= 1);
-const isIncreaseDisabled = computed(
-  () => props.modelValue >= computedTotalPages.value,
-);
-
-const getTotalPagesArray = () => {
-  const pages = [];
-  for (let page = 1; page <= computedTotalPages.value; page++) {
-    pages.push(page);
-  }
-  return pages;
-};
-
-const getPagesWithoutSlice = () => {
-  const pages = [];
-  const slicedLength =
-    Math.abs(props.modelValue - props.sliceLength) +
-    props.modelValue +
-    props.sliceLength +
-    1;
-  for (let page = 1; page <= slicedLength; page++) {
-    pages.push(page);
-  }
-  return pages;
-};
-
-const getEndOfPagination = () => {
-  const pages = [];
-  for (
-    let page = Math.abs(computedTotalPages.value - props.sliceLength * 2);
-    page <= computedTotalPages.value;
-    page++
-  ) {
-    pages.push(page);
-  }
-  return pages;
-};
-
-const getPagesWithNewView = () => {
-  const pages = [];
-  let startedPage =
-    props.modelValue - props.sliceLength > 0
-      ? props.modelValue - props.sliceLength
-      : 1;
-  for (
-    let page = startedPage;
-    page < props.modelValue + props.sliceLength + 1;
-    page++
-  ) {
-    if (page >= computedTotalPages.value) break;
-    pages.push(page);
-  }
-  return pages;
-};
-
-const pagesToDisplay = computed(() => {
-  if (props.layout === 'navigation') return [];
-  if (props.layout === 'table') return [];
-
-  const sliceLengthCoverAllPages =
-    computedTotalPages.value <= props.sliceLength * 2 + 1;
-  if (sliceLengthCoverAllPages) return getTotalPagesArray();
-
-  const withoutSliceLength = props.modelValue <= props.sliceLength;
-  if (withoutSliceLength) return getPagesWithoutSlice();
-
-  const endOfPagination =
-    props.modelValue >= computedTotalPages.value - props.sliceLength;
-  if (endOfPagination) return getEndOfPagination();
-
-  return getPagesWithNewView();
-});
-
-const startItemsCount = computed(
-  () => props.modelValue * props.perPage - props.perPage + 1,
-);
-const endItemsCount = computed(() => {
-  const count = props.modelValue * props.perPage + 1;
-  if (!props.totalItems) return count;
-  if (count > props.totalItems) return props.totalItems;
-
-  return count;
-});
-const computedTotalItems = computed(() => {
-  if (props.totalItems) return props.totalItems;
-
-  return computedTotalPages.value * props.perPage;
 });
 </script>

cc729aef1489ee7e31850b0acb0cc66bc29c7e8c
Roman Korniienko <roman.korniienko@belkins.io>
2023-05-08 23:56:26 +0300

    test(SkillItem.spec.js): add test for computed classes;

--- tests/unit/SkillItem.spec.js
+++ tests/unit/SkillItem.spec.js
@@ -2,16 +2,58 @@ import { mount } from '@vue/test-utils';
 import SkillItem from '@/components/pages/course/lesson/SkillItem.vue';
 
 describe('SkillItem', () => {
+  const skillName = 'Self-improvement'
   it('should display appropriate prop name', () => {
-    const skillName = 'Self-improvement'
 
     const wrapper = mount(SkillItem, {
       props: {
-        name: skillName
+        name: skillName,
+        index: 0
       },
     });
 
     expect(wrapper.text()).toBe(skillName)
+  })
+
+  it('should render span', () => {
+    const wrapper = mount(SkillItem, {
+      props: {
+        name: skillName,
+        index: 0
+      },
+    });
+
+    expect(wrapper.find('span').exists()).toBe(true);
+  })
+
+  it('should display appropriate dynamic class', () => {
+    const wrapper = mount(SkillItem, {
+      props: {
+        name: skillName,
+        index: 0
+      },
+    });
+
+    const greenIndex = [0, 5]
+    const yellowIndex = [1, 6]
+    const redIndex = [2, 7]
+    const orangeIndex = [3, 8]
+    const blueIndex = [4, 9]
+
+    const cssMap = {
+      [greenIndex]: 'bg-green-100 text-green-800',
+      [yellowIndex]: 'bg-green-100 text-green-800',
+      [redIndex]: 'bg-green-100 text-green-800',
+      [orangeIndex]: 'bg-green-100 text-green-800',
+      [blueIndex]: 'bg-green-100 text-green-800',
+    }
+
+    Object.entries(cssMap).forEach(([key, value]) => {
+      wrapper.setProps({ 'index': key[0] })
+
+      expect(wrapper.vm.dynamicClasses).toBe(value)
+    })
+
   })
 })
 

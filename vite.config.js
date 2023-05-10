2b7f8b8d68cf5dd10b936b4a4ced284c85cb751d
Roman Korniienko <roman.korniienko@belkins.io>
2023-05-11 00:52:33 +0300

    test(env): fix env problem;

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,12 @@ import svgLoader from 'vite-svg-loader';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  define: {
+    'process.env': {
+      'VITE_API_BASE_URL': 'https://api.wisey.app',
+      'VITE_API_VERSION': 'api/v1'
+    }
+  },
   server: {
     port: 8080,
   },
